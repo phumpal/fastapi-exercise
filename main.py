9ef56c0d7b9830435127f927329ddbbd2b7afb7b
Patrick Humpal <654902+phumpal@users.noreply.github.com>
2025-04-14 00:45:02 -0500

    fastapi, pydantic, sqlaclhemy
    Create the database connection using SQlAlchmey and initialize an ItemDB
    model that maps to an "items" table. Per the requirements
    
    - id: is a unique identifier and the primary key
    - content: a string with an arbitrary payload

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-#!/ur/bin/env python
+#!/usr/bin/env python
+
+import os
 
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+from pydantic import BaseModel
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class ItemDB(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+class Item(BaseModel):
+    id: int
+    content: str
+
+    model_config = {"from_attributes": True}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -10,5 +44,5 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None, db: Session = Depends(get_db)):
     return {"item_id": item_id, "q": q}
