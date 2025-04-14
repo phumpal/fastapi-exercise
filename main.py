43ae0093a0c4108cd707e0a3aa0ea6997bdb5f57
Patrick Humpal <654902+phumpal@users.noreply.github.com>
2025-04-14 01:21:34 -0500

    fastapi: adds DB functionality
    - Prefer use of anchor env vars to construct DB connection string
    - POST /items - adds a new item
    - GET /items - returns a list of items

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
-
 import os
 
-from typing import Union
-from fastapi import FastAPI, Depends
+from typing import Union, List
+from fastapi import FastAPI, Depends, HTTPException
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.engine.url import URL
 
 from pydantic import BaseModel
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = str(URL.create(
+        drivername="postgresql",
+        username=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        database=os.getenv("POSTGRES_DB"),
+    ))
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
@@ -24,9 +35,14 @@ class ItemDB(Base):
 
 Base.metadata.create_all(bind=engine)
 
-class Item(BaseModel):
-    id: int
+class ItemBase(BaseModel):
     content: str
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
 
     model_config = {"from_attributes": True}
 
@@ -43,6 +59,22 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None, db: Session = Depends(get_db)):
-    return {"item_id": item_id, "q": q}
+@app.post("/items/", response_model=Item)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = ItemDB(content=item.content)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.get("/items/{item_id}", response_model=Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@app.get("/items/", response_model=List[Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = db.query(ItemDB).offset(skip).limit(limit).all()
+    return items
